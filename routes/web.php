3c05870fdd75569decb14dfdad7ed4ee09e5994b
Zulfikar Azmi <zazmialghifari@gmail.com>
2024-10-09 15:28:34 +0700

    menambah kode login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/create-code', [ClientController::class, 'store'])->name('create.code');
+
 
 require __DIR__.'/auth.php';
